7c09e814f46e604c1ba2eef3e933d30526e6739e
PrimeRick <118068924+PrimeRick@users.noreply.github.com>
2023-07-13 22:17:30 +0530

    Update anagram.js

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let cc=0;
+  for(let i=0; i<str1.length(); i++){
+    for(let j=0; j<str2.legth(); j++){
+      if(str1[i]==str[j]){
+        cc++;
+      }
+    }
+  }
+  if(cc==str1.length()){
+    return true;
+  }
+  return false;
 }
 
 module.exports = isAnagram;
+npm run test-anagram
